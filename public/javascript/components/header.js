ec9f8efdc0e9c9568137cece72f4b1ebe61a1698
Lucy <lucymidgley13@gmail.com>
2020-02-08 22:12:49 +0000

    added investment button

--- public/javascript/components/header.js
+++ public/javascript/components/header.js
@@ -21,6 +21,7 @@ $(() => {
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
+          <button type="button" class="btn btn-primary">Invest now!</button>
         </nav>
       </div>`
 
