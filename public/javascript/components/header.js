c9a0b56032ad31b4f028cdb504e458ca225625a0
Lucy <lucymidgley13@gmail.com>
2020-02-08 21:38:20 +0000

    somthing

--- public/javascript/components/header.js
+++ public/javascript/components/header.js
@@ -1,28 +1,29 @@
 $(() => {
+  console.log('window ready')
   window.header = {};
 
   const $pageHeader = $('#page-header');
-  let currentUser = null;
-  function updateHeader(user) {
-    currentUser = user;
-    $pageHeader.find("#page-header__user-links").remove();
+  //let currentUser = null;
+  function updateHeader() {
+    //currentUser = user;
+   // $pageHeader.find("#page-header__user-links").remove();
     let userLinks;
-
-    if (!user) {
-      userLinks = ``
-    } else {
-      userLinks = `
-      <li>${user.name}</li>`
-    }
+userlinks=`<h1>this is a geader<h1>`
+    // if (!user) {
+    //   userLinks = ``
+    // } else {
+    //   userLinks = `
+    //   <li>${user.name}</li>`
+    // }
 
     $pageHeader.append(userLinks);
   }
 
   window.header.update = updateHeader;
 
-  getMyDetails()
-    .then(function( json ) {
-    updateHeader(json.user);
-  });
-
+  // getMyDetails()
+  //   .then(function( json ) {
+  //   updateHeader(json.user);
+  // });
+updateHeader();
 });
