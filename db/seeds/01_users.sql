b8b0b08f205cdf672c0b03128cd0c92ba07adaf8
Ken Fyfe <kenfyfe@kenfyfe.com>
2020-02-09 01:12:15 +0000

    fixed up one password

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -6,7 +6,7 @@ VALUES('janeeyre', 'jane@email.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh
  ('daffyduck', 'daffy@email.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
  ('bugsbunny', 'bugs@email.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
  ('sybilfawlty', 'sybil@email.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
- ('thebfg', 'thebfg@email.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'.),
+ ('thebfg', 'thebfg@email.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
  ('jamesandthegiantpeach', 'peachy@email.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
  ('charliebucket', 'charlie@email.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
  ('captainmannering', 'mannering@email.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
