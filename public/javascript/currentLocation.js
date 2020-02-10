67d7f3e97358586758f38b6333566155a048c596
Ken Fyfe <kenfyfe@kenfyfe.com>
2020-02-10 18:54:33 +0000

    added getMyPins ajax functionality

--- public/javascript/currentLocation.js
+++ public/javascript/currentLocation.js
@@ -1,3 +1,4 @@
+
 const currentLocation = function() {
   const map = L.map("map-current-location");
 
@@ -6,22 +7,22 @@ const currentLocation = function() {
     {
       maxZoom: 18,
       attribution:
-        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-        '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+      '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+      'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
       id: "mapbox/streets-v11"
     }
-  ).addTo(map);
+    ).addTo(map);
 
-  const onLocationFound = function(e) {
-    L.marker(e.latlng).addTo(map);
+    const onLocationFound = function(e) {
+      L.marker(e.latlng).addTo(map);
+    };
+
+    const onLocationError = function(e) {
+      alert(e.message);
+    };
+
+    map.on("locationfound", onLocationFound);
+    map.on("locationerror", onLocationError);
+    map.locate({ setView: true, maxZoom: 16 })
   };
-
-  const onLocationError = function(e) {
-    alert(e.message);
-  };
-
-  map.on("locationfound", onLocationFound);
-  map.on("locationerror", onLocationError);
-  map.locate({ setView: true, maxZoom: 16 });
-};
