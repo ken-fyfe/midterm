f96a7a26d6b14f1e5b3ae32fc98f8d31b84b0c8c
Lucy <lucymidgley13@gmail.com>
2020-02-27 02:53:49 +0000

    tried playing with size of map

--- public/javascript/currentMap.js
+++ public/javascript/currentMap.js
@@ -33,16 +33,16 @@ $(() => {
   }));	//base layer
 
 
-  // map.addControl( new L.Control.Search({
-	// 	url: 'https://nominatim.openstreetmap.org/search?format=json&q={s}',
-	// 	jsonpParam: 'json_callback',
-	// 	propertyName: 'display_name',
-	// 	propertyLoc: ['lat','lon'],
-	// 	marker: L.circleMarker([0, 0], {radius: 30}),
-	// 	autoCollapse: true,
-	// 	autoType: false,
-	// 	minLength: 2
-  // }));
+  map.addControl( new L.Control.Search({
+		url: 'https://nominatim.openstreetmap.org/search?format=json&q={s}',
+		jsonpParam: 'json_callback',
+		propertyName: 'display_name',
+		propertyLoc: ['lat','lon'],
+		marker: L.circleMarker([0, 0], {radius: 30}),
+		autoCollapse: true,
+		autoType: false,
+		minLength: 2
+  }));
 
   const onLocationFound = function(e) {
     L.marker(e.latlng).addTo(map);
