0c557c73ed84ee4924d3042fa9fa9f488eedb5ab
Lucy <lucymidgley13@gmail.com>
2020-02-26 18:25:05 -0700

    updated descriptions of pins

--- db/seeds/03_pins.sql
+++ db/seeds/03_pins.sql
@@ -1,9 +1,9 @@
 INSERT INTO pins (
   user_id, title, latitude, longitude, description, category)
-  VALUES(1, 'Darwen tower', 53.695450, -2.452070, 'some text', 'cultural'),
-  (1, 'Darwen library', 53.696730, -2.466284, 'some text', 'cultural'),
-  (1, 'Darwen Mill chimney', 53.690926, -2.465221, 'some text', 'cultural'),
-  (4, 'Eiffel Tower', 48.858588, 2.294128, 'some text', 'cultural'),
-  (1, 'Pantheon', 48.846363, 2.347015, 'some text', 'cultural'),
-  (6, 'Louvre', 48.860579, 2.337676, 'some text', 'cultural'),
-  (1, 'Sacre coeur', 48.887259, 2.342975, 'some text', 'cultural');
+  VALUES(1, 'Darwen tower', 53.695450, -2.452070, 'Beautiful Views!', 'cultural'),
+  (1, 'Darwen library', 53.696730, -2.466284, 'Lovely architecture', 'cultural'),
+  (1, 'Darwen Mill chimney', 53.690926, -2.465221, 'Nice Chimney', 'cultural'),
+  (4, 'Eiffel Tower', 48.858588, 2.294128, 'Good views', 'cultural'),
+  (1, 'Pantheon', 48.846363, 2.347015, 'Lovely dome', 'cultural'),
+  (6, 'Louvre', 48.860579, 2.337676, 'Mona Lisa!', 'cultural'),
+  (1, 'Sacre coeur', 48.887259, 2.342975, 'Beautiful church', 'cultural');
